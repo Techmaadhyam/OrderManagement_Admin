04216a4c5674a0fd4d849a40550db9f6cd4b62c5
Mithesh B <mitheshbangera70@gmail.com>
2023-04-28 15:27:04 +0530

    create inventory

---
+++ src/pages/dashboard/inventory/create.js
@@ -0,0 +1,71 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import PlusIcon from '@untitled-ui/icons-react/build/esm/Plus';
+import { Box, Button, Container, Divider, Stack, SvgIcon, Typography } from '@mui/material';
+import { ordersApi } from 'src/api/orders';
+import { customersApi } from 'src/api/customers';
+import { Seo } from 'src/components/seo';
+import { useDialog } from 'src/hooks/use-dialog';
+import { useMounted } from 'src/hooks/use-mounted';
+import { usePageView } from 'src/hooks/use-page-view';
+import { OrderDrawer } from 'src/sections/dashboard/order/order-drawer';
+import { OrderListContainer } from 'src/sections/dashboard/order/order-list-container';
+import { OrderListSearch } from 'src/sections/dashboard/order/order-list-search';
+import { OrderListTable } from 'src/sections/dashboard/order/order-list-table';
+import { CreateInventory } from 'src/sections/dashboard/inventory/create-inventory';
+
+const useCustomer = () => {
+  const isMounted = useMounted();
+  const [customer, setCustomer] = useState(null);
+
+  const handleCustomerGet = useCallback(async () => {
+    try {
+      const response = await customersApi.getCustomer();
+
+      if (isMounted()) {
+        setCustomer(response);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }, [isMounted]);
+
+  useEffect(() => {
+      handleCustomerGet();
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []);
+
+  return customer;
+};
+
+
+const Page = () => {
+  const customer = useCustomer();
+
+  usePageView();
+
+  if (!customer) {
+    return null;
+  }
+
+  return (
+    <>
+      <Seo title="Dashboard: Customer Edit" />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8
+        }}
+      >
+        <Container maxWidth="lg">
+          <Stack spacing={4}>
+            <CreateInventory customer={customer} />
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Page;
