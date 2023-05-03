e1729d2f4b04219e7c3b63d25eb4d751fbffef81
Mithesh <mitheshbangera70@gmail.com>
2023-05-03 15:18:06 +0530

    add country, state, city selection

--- src/pages/auth/jwt/register.js
+++ src/pages/auth/jwt/register.js
@@ -21,38 +21,95 @@ import { usePageView } from 'src/hooks/use-page-view';
 import { useSearchParams } from 'src/hooks/use-search-params';
 import { paths } from 'src/paths';
 import { AuthIssuer } from 'src/sections/auth/auth-issuer';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { primaryColor } from 'src/primaryColor'; 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-const userOptions = [
-  {
-    label: 'Healthcare',
-    value: 'healthcare'
-  },
-  {
-    label: 'Makeup',
-    value: 'makeup'
-  },
-  {
-    label: 'Dress',
-    value: 'dress'
-  },
-  {
-    label: 'Skincare',
-    value: 'skincare'
-  },
-  {
-    label: 'Jewelry',
-    value: 'jewelry'
-  },
- 
-];
 
 const Register = () => {
   const [currentImage, setCurrentImage] = useState(0);
+  const [accessToken, setAccessToken] = useState(null);
+  const [error, setError] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [states, setStates]= useState([])
+  const [cities, setCities]= useState([])
+  const [currentCountry, setCurrentCountry]= useState(['India'])
+  const [currentState, setCurrentState]= useState([])
+  const [currentCity, setCurrentCity] =useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://www.universal-tutorial.com/api/getaccesstoken', {
+          headers: {
+            'Accept': 'application/json',
+            'api-token': '8HWETQvEFegKi6tGPUkSWDiQKfW8UdZxPqbzHX6JdShA3YShkrgKuHUbnTMkd11QGkE',
+            'user-email': 'mithesh.dev.work@gmail.com'
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch access token');
+        }
+
+        const data = await response.json();
+
+        setAccessToken(data.auth_token);
+        setError(null);
+      } catch (error) {
+        console.error(error);
+        setError('Failed to fetch access token');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const fetchCountries = useCallback(async () => {
+    try {
+      const response = await fetch("https://www.universal-tutorial.com/api/countries/", {
+        headers: {
+          "Authorization": "Bearer " + accessToken,
+          "Accept": "application/json"
+        }
+      });
+  
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.error("Error fetching countries:", error);
+    }
+  }, [accessToken]);
+
+  useEffect(()=>{
+    fetchCountries()
+  },[fetchCountries])
+
+  const userOptions = useMemo(() => {
+    return countries.map(country => ({
+      label: country.country_name,
+      value: country.country_name
+    }));
+  }, [countries]);
+
+  const userOptionsState = useMemo(() => {
+    return states.map(state => ({
+      label: state.state_name,
+      value: state.state_name
+    }));
+  }, [states]);
+  const userOptionsCities = useMemo(() => {
+    return cities.map(city => ({
+      label: city.city_name,
+      value: city.city_name
+    }));
+  }, [cities]);
+
   const notify = () => toast.success('You have sucessfully registered your account', {
     position: "top-right",
     autoClose: 2000,
@@ -64,7 +121,49 @@ const Register = () => {
     theme: "light",
     });
 
+    const handleCountry = async (event) => {
+      try {
+        setCurrentCountry(event.target.value);
+        const response = await fetch(`https://www.universal-tutorial.com/api/states/${event.target.value}`, {
+          headers: {
+            "Authorization": "Bearer " + accessToken,
+            "Accept": "application/json"
+          }
+        });
+  
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setStates(data);
+      } catch (error) {
+        console.error("Error fetching states:", error);
+      }
+    };
 
+const handleState = async (event) => {
+      try {
+        setCurrentState(event.target.value);
+        const response = await fetch(`https://www.universal-tutorial.com/api/cities/${event.target.value}`, {
+          headers: {
+            "Authorization": "Bearer " + accessToken,
+            "Accept": "application/json"
+          }
+        });
+  
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setCities(data);
+      } catch (error) {
+        console.error("Error fetching states:", error);
+      }
+};
+
+const handleCities = async (event) => {
+    setCurrentCity(event.target.value);
+}
   const images = [
     '/assets/logos/logo.png',
     '/assets/logos/logo1.png',
@@ -79,18 +178,19 @@ const Register = () => {
     '/assets/logos/logo10.png',  
   ];
 
+  const handleImageChange = useCallback(() => {
+    setCurrentImage(currentImage => (currentImage + 1) % images.length);
+  }, [images.length]);
+  
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentImage(currentImage => (currentImage + 1) % images.length);
-    }, 3000);
-    
+    const intervalId = setInterval(handleImageChange, 3000);
     return () => clearInterval(intervalId);
-  }, []);
-
+  }, [handleImageChange]);
 
   const handleToHome =() => {
       notify();
   }
+ 
 
   return (
    <>
@@ -330,10 +430,12 @@ const Register = () => {
                     fullWidth
                     label="Country"
                     name="country"
-                    select 
-                    defaultValue=""             
+                    select
+                    defaultValue= ''
+                    value={currentCountry}
+                    onChange={handleCountry}
                   >
-                    {userOptions.map((option) => (
+                    {userOptions?.map((option) => (
                       <MenuItem
                         key={option.value}
                         value={option.value}
@@ -352,9 +454,12 @@ const Register = () => {
                     label="State"
                     name="state"
                     select
-                    defaultValue=""
+                    defaultValue=''
+                    value={currentState}
+                    onChange={handleState}
+                   
                 > 
-                {userOptions.map((option) => (
+                {userOptionsState?.map((option) => (
                       <MenuItem
                         key={option.value}
                         value={option.value}
@@ -373,9 +478,11 @@ const Register = () => {
                 label="City"
                 name="city"
                 select
-                defaultValue=""
+                defaultValue=''
+                value={currentCity}
+                onChange={handleCities}
               >
-                  {userOptions.map((option) => (
+                  {userOptionsCities?.map((option) => (
                       <MenuItem
                         key={option.value}
                         value={option.value}
