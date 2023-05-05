65be1cfd4c432aa83df9895f2b043d1b8454b2da
Mithesh <mitheshbangera70@gmail.com>
2023-05-05 17:49:51 +0530

    create user profile

---
+++ src/sections/dashboard/user/user-icon.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import User03 from '@untitled-ui/icons-react/build/esm/User03';
+import { ButtonBase, SvgIcon, Box, Link } from '@mui/material';
+import './user.css';
+import { paths } from 'src/paths';
+import { RouterLink } from 'src/components/router-link';
+import { primaryColor } from 'src/primaryColor';
+
+const User = () => {
+  return (
+    <Link style={{color: '#111927'}}
+    component={RouterLink}
+    href= {paths.dashboard.social.profile}>
+      <Box className="popup-container" sx={{backgroundColor: `${primaryColor}`}}
+        >
+        <ButtonBase className="popup-trigger">
+          <SvgIcon >
+            <User03 style={{color: '#fff'}} />
+          </SvgIcon>
+        </ButtonBase>
+        <div className="popup-content">
+          <span className='title'>Tech Maadhyam</span>
+          <span className='user'>User: Max Ray</span>
+          <span className='mail'>Mail: maxray@xyz.com</span>
+        </div>
+      </Box>
+    </Link>
+  );
+}
+
+export default User;
+
