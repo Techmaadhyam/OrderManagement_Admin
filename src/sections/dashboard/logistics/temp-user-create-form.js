be2e24cc7d1f3cb814a5397f54017be0a701cd6c
Mithesh <mitheshbangera70@gmail.com>
2023-05-05 09:50:20 +0530

    create temp user form

---
+++ src/sections/dashboard/logistics/temp-user-create-form.js
@@ -0,0 +1,339 @@
+import PropTypes from 'prop-types';
+import toast from 'react-hot-toast';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  TextField,
+  MenuItem,
+  Unstable_Grid2 as Grid
+} from '@mui/material';
+import { DatePicker } from 'antd';
+import { wait } from 'src/utils/wait';
+import { Box } from '@mui/system';
+
+const userOptions = [
+  {
+    label: 'Healthcare',
+    value: 'healthcare'
+  },
+  {
+    label: 'Makeup',
+    value: 'makeup'
+  },
+  {
+    label: 'Dress',
+    value: 'dress'
+  },
+  {
+    label: 'Skincare',
+    value: 'skincare'
+  },
+  {
+    label: 'Jewelry',
+    value: 'jewelry'
+  },
+  {
+    label: 'Blouse',
+    value: 'blouse'
+  }
+];
+
+export const TempUserCreateForm = (props) => {
+  const { customer, ...other } = props;
+  const formik = useFormik({
+    initialValues: {
+      address1: customer.address1 || '',
+      address2: customer.address2 || '',
+      country: customer.country || '',
+      email: customer.email || '',
+      hasDiscount: customer.hasDiscount || false,
+      isVerified: customer.isVerified || false,
+      name: customer.name || '',
+      phone: customer.phone || '',
+      state: customer.state || '',
+      submit: null
+    },
+    validationSchema: Yup.object({
+      address1: Yup.string().max(255),
+      address2: Yup.string().max(255),
+      country: Yup.string().max(255),
+      email: Yup
+        .string()
+        .email('Must be a valid email')
+        .max(255)
+        .required('Email is required'),
+      hasDiscount: Yup.bool(),
+      isVerified: Yup.bool(),
+      name: Yup
+        .string()
+        .max(255)
+        .required('Name is required'),
+      phone: Yup.string().max(15),
+      state: Yup.string().max(255)
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        // NOTE: Make API request
+        await wait(500);
+        helpers.setStatus({ success: true });
+        helpers.setSubmitting(false);
+        toast.success('Customer updated');
+      } catch (err) {
+        console.error(err);
+        toast.error('Something went wrong!');
+        helpers.setStatus({ success: false });
+        helpers.setErrors({ submit: err.message });
+        helpers.setSubmitting(false);
+      }
+    }
+  });
+
+  return (
+    <div style={{minWidth: "100%" , marginBottom: '1rem' }}>
+    <h2>Create Temporary User</h2>
+    <form
+      onSubmit={formik.handleSubmit}
+      {...other}>
+      <Card>
+        <CardHeader title="New Temporary User" />
+        <CardContent sx={{ pt: 0 }}>
+          <Grid
+            container
+            spacing={3}
+          >
+              <Grid
+              xs={12}
+              md={6}
+            >
+              <TextField
+                error={!!(formik.touched.address1 && formik.errors.address1)}
+                fullWidth
+                helperText={formik.touched.address1 && formik.errors.address1}
+                label="First Name"
+                name="firstname"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+
+              />
+            </Grid>
+            <Grid
+              xs={12}
+              md={6}
+            >
+              <TextField
+                error={!!(formik.touched.address1 && formik.errors.address1)}
+                fullWidth
+                helperText={formik.touched.address1 && formik.errors.address1}
+                label="Last Name"
+                name="lastame"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+         
+              />
+            </Grid>
+            <Grid
+              xs={12}
+              md={6}
+            >
+              <TextField
+                error={!!(formik.touched.address1 && formik.errors.address1)}
+                fullWidth
+                helperText={formik.touched.address1 && formik.errors.address1}
+                label="Email"
+                name="email"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+             
+              />
+            </Grid>
+            <Grid
+              xs={12}
+              md={6}
+            >
+              <TextField
+                error={!!(formik.touched.address1 && formik.errors.address1)}
+                fullWidth
+                helperText={formik.touched.address1 && formik.errors.address1}
+                label="Phone"
+                name="phone"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+
+              />
+            </Grid>
+            <Grid
+              xs={12}
+              md={6}
+            >
+              <TextField
+                error={!!(formik.touched.address1 && formik.errors.address1)}
+                fullWidth
+                helperText={formik.touched.address1 && formik.errors.address1}
+                label="Type"
+                name="Type"
+                select
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+              >
+              {userOptions.map((option) => (
+                <MenuItem
+                  key={option.value}
+                  value={option.value}
+                >
+                  {option.label}
+                </MenuItem>
+              ))}
+              </TextField>
+            </Grid>
+            <Grid
+              xs={12}
+              md={6}
+            >
+              <TextField
+                error={!!(formik.touched.address1 && formik.errors.address1)}
+                fullWidth
+                helperText={formik.touched.address1 && formik.errors.address1}
+                label="Company"
+                name="company"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+
+              />
+            </Grid>
+            <Grid
+              xs={12}
+              md={12}
+            >
+              <TextField
+                error={!!(formik.touched.address1 && formik.errors.address1)}
+                fullWidth
+                helperText={formik.touched.address1 && formik.errors.address1}
+                label="Address"
+                multiline
+                minRows={3}
+                name="address"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+
+              />
+            </Grid>
+            <Grid
+              xs={12}
+              md={12}
+            >
+              <TextField
+                    error={!!(formik.touched.category && formik.errors.category)}
+                    fullWidth
+                    label="Country"
+                    name="country"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    select
+                    value={formik.values.category}
+                  >
+                    {userOptions.map((option) => (
+                      <MenuItem
+                        key={option.value}
+                        value={option.value}
+                      >
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+            </Grid>
+            <Grid
+              xs={12}
+              md={12}
+            >
+                <TextField
+                    error={!!(formik.touched.category && formik.errors.category)}
+                    fullWidth
+                    label="State"
+                    name="state"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    select
+                    value={formik.values.category}
+                  >
+                    {userOptions.map((option) => (
+                      <MenuItem
+                        key={option.value}
+                        value={option.value}
+                      >
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+            </Grid>
+            <Grid
+              xs={12}
+              md={12}
+            >
+               <TextField
+                    error={!!(formik.touched.category && formik.errors.category)}
+                    fullWidth
+                    label="City"
+                    name="city"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    select
+                    value={formik.values.category}
+                  >
+                    {userOptions.map((option) => (
+                      <MenuItem
+                        key={option.value}
+                        value={option.value}
+                      >
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+            </Grid>
+        
+            <Grid
+              xs={12}
+              md={12}
+            >
+              <TextField
+                error={!!(formik.touched.address1 && formik.errors.address1)}
+                fullWidth
+                helperText={formik.touched.address1 && formik.errors.address1}
+                label="ZipCode"
+                name="zipcode"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+              />
+            </Grid>
+          </Grid>
+        </CardContent>
+        <Divider/>
+      </Card>
+    </form>
+    <Grid
+              xs={12}
+              md={6}
+            >
+            <Box sx={{ mt: 2 }}
+            display="flex"
+            justifyContent="flex-end">
+          <Button
+            color="primary"
+            variant="contained"
+            align="right"
+          >
+            Save
+          </Button>
+        </Box>
+          </Grid>
+    </div>
+  );
+};
+
+TempUserCreateForm.propTypes = {
+  customer: PropTypes.object.isRequired
+};
