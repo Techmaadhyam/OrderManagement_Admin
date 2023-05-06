64d4d7738588b5c15bb383f6a33d336dc6abde75
Mithesh <mitheshbangera70@gmail.com>
2023-05-06 15:52:29 +0530

    create view layout for warehouse

---
+++ src/sections/dashboard/invoice/view-warehouse-detail.js
@@ -0,0 +1,214 @@
+import PropTypes from 'prop-types';
+import toast from 'react-hot-toast';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import ArrowLeftIcon from '@untitled-ui/icons-react/build/esm/ArrowLeft';
+import {
+  Button,
+  Card,
+  CardHeader,
+  Divider,
+  TextField,
+  Typography,
+  Link,
+  SvgIcon,
+  IconButton,
+  Grid
+} from '@mui/material';
+import { wait } from 'src/utils/wait';
+import {  Box, Stack } from '@mui/system';
+import { PropertyList } from 'src/components/property-list';
+import { PropertyListItem } from 'src/components/property-list-item';
+import { useCallback, useState } from 'react';
+import { RouterLink } from 'src/components/router-link';
+import { paths } from 'src/paths';
+import { Scrollbar } from 'src/components/scrollbar';
+import { Table } from 'antd';
+import { primaryColor } from 'src/primaryColor';
+import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+
+
+const statusOptions = ['Canceled', 'Complete', 'Rejected'];
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  {
+    title: 'Zip Code',
+    dataIndex: 'zipcode',
+    key: 'zipcode',
+  },
+  
+  {
+    title: 'Description',
+    key: 'description',
+    dataIndex: 'description',
+  },
+
+];
+
+const rowData = [
+    {
+        name: 'Max Ray',
+        address: "Street: 277, I S Sadan X Road, Santoshnagar, Hyderabad, Andhra Pradesh, India",
+        zipcode: '30827',
+        description: 'testing random description',
+      },
+];
+
+const data = 
+{
+    name: 'Max Ray',
+    address: "Street: 277, I S Sadan X Road, Santoshnagar, Hyderabad, Andhra Pradesh, India",
+    zipcode: '30827',
+    description: 'testing random description',
+}
+
+
+
+export const ViewWarehouseDetail = (props) => {
+  const { customer, ...other } = props;
+  const [status, setStatus] = useState(statusOptions[0]);
+
+  const handleChange = useCallback((event) => {
+    setStatus(event.target.value);
+  }, []);
+  const align = 'horizontal' 
+  const formik = useFormik({
+    initialValues: {
+      address1: customer.address1 || '',
+      address2: customer.address2 || '',
+      country: customer.country || '',
+      email: customer.email || '',
+      hasDiscount: customer.hasDiscount || false,
+      isVerified: customer.isVerified || false,
+      name: customer.name || '',
+      phone: customer.phone || '',
+      state: customer.state || '',
+      submit: null
+    },
+    validationSchema: Yup.object({
+      address1: Yup.string().max(255),
+      address2: Yup.string().max(255),
+      country: Yup.string().max(255),
+      email: Yup
+        .string()
+        .email('Must be a valid email')
+        .max(255)
+        .required('Email is required'),
+      hasDiscount: Yup.bool(),
+      isVerified: Yup.bool(),
+      name: Yup
+        .string()
+        .max(255)
+        .required('Name is required'),
+      phone: Yup.string().max(15),
+      state: Yup.string().max(255)
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        // NOTE: Make API request
+        await wait(500);
+        helpers.setStatus({ success: true });
+        helpers.setSubmitting(false);
+        toast.success('Customer updated');
+      } catch (err) {
+        console.error(err);
+        toast.error('Something went wrong!');
+        helpers.setStatus({ success: false });
+        helpers.setErrors({ submit: err.message });
+        helpers.setSubmitting(false);
+      }
+    }
+  });
+
+  return (
+    <div style={{minWidth: "100%", marginTop: "1rem"  }}>
+      <div>
+          <Link
+          color="text.primary"
+          component={RouterLink}
+          href={paths.dashboard.invoices.details}
+          sx={{
+            alignItems: 'center',
+            display: 'inline-flex',
+          }}
+          underline="none"
+        >
+          <SvgIcon sx={{ mr: 1, width: 38, height: 38,  transition: 'color 0.5s','&:hover': { color: `${primaryColor}` }}}>
+            <ArrowCircleLeftOutlinedIcon/>
+          </SvgIcon>
+          <Typography variant="subtitle2">
+            Warehouse <span style={{color: `${primaryColor}` , fontWeight: 600}}>List</span> 
+          </Typography>
+        </Link>
+      </div>
+ <h2>Warehouse</h2>
+      <Card style={{marginBottom: "12px" }}>
+        <CardHeader title="Warehouse Detail" />
+        <PropertyList>
+        <PropertyListItem
+          align={align}
+          label="Name"
+        >
+          <Typography variant="subtitle2">
+            {data.name}
+          </Typography>
+        </PropertyListItem>
+        <Divider />
+        <PropertyListItem
+          align={align}
+          label="Address"
+          value={data.address}
+        />
+        <Divider />
+        <PropertyListItem
+          align={align}
+          label="Zip Code"
+          value={data.zipcode}
+        />
+        <Divider />
+        <PropertyListItem
+          align={align}
+          label="Description"
+          value={data.description}
+        />
+      </PropertyList>
+        <Divider/>
+      </Card>
+      <Card style={{marginBottom: "40px" }}>
+      <Box sx={{  position: 'relative' , overflowX: "auto", marginBottom: '30px'}}>    
+      <Scrollbar>
+        <Table sx={{ minWidth: 800,overflowX: "auto" }} pagination={false} columns={columns} dataSource={rowData}></Table>
+      </Scrollbar>
+    </Box>
+     <Grid
+              xs={12}
+              md={6}
+            >
+  <Typography style={{ fontFamily:"Arial, Helvetica, sans-serif", fontSize:"14px", marginRight: '6px', color:'black', fontWeight:"bold"}}>Total Amount : 56,78,020</Typography>
+            </Grid>
+            <Grid
+              xs={12}
+              md={6}
+              style={{marginTop: "20px", marginBottom: "30px"}}
+            >
+  <Typography style={{ fontFamily:"Arial, Helvetica, sans-serif", fontSize:"14px", marginRight: '6px', color:'black', fontWeight:"bold"}}>Terms &Conditions :  This product can be sold on the said customer</Typography>
+
+            </Grid>
+        <Divider/>
+      </Card>
+    </div>
+  );
+};
+
+ViewWarehouseDetail.propTypes = {
+  customer: PropTypes.object.isRequired
+};
---
+++ src/sections/dashboard/invoice/view-warehouse.js
@@ -0,0 +1,171 @@
+import PropTypes from 'prop-types';
+import {
+  Unstable_Grid2 as Grid,
+  Typography,
+  IconButton,
+  Icon,
+  Link
+} from '@mui/material';
+import { Table } from 'antd';
+import { Box } from '@mui/system';
+import React from 'react';
+import { Scrollbar } from 'src/components/scrollbar';
+import EditIcon from '@mui/icons-material/Edit';
+import {  Delete } from '@mui/icons-material';
+import { RouterLink } from 'src/components/router-link';
+import { paths } from 'src/paths';
+
+
+const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (name) => <Link
+      color="primary"
+      component={RouterLink}
+      href={paths.dashboard.invoices.viewDetail}
+      sx={{
+        alignItems: 'center',
+        textAlign: 'center'
+        // display: 'inline-flex'
+      }}
+      underline="hover"
+    >
+      <Typography variant="subtitle2">
+     {name}
+      </Typography>
+    </Link>
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+      },
+    {
+      title: 'Zip Code',
+      dataIndex: 'zipcode',
+      key: 'zipcode',
+    },
+    
+    {
+      title: 'Description',
+      key: 'description',
+      dataIndex: 'description',
+    },
+
+      {
+        dataIndex: 'actionEdit',
+        key: 'actionEdit',
+      render: () => <Link
+   
+    >
+      <IconButton>
+    <Icon>
+           <EditIcon />
+       </Icon>
+       </IconButton>
+    </Link>
+      },
+      {
+        dataIndex: 'actionDelete',
+        key: 'actionDelete',
+        render: (_, __, index) =>  <IconButton
+         onClick={() => this.handleRemoveRow(index)}
+       >
+         <Icon>
+           <Delete />
+         </Icon>
+       </IconButton>
+      },
+  ];
+  
+  const data = [
+    {
+      key: '1',
+      name: 'Max Ray',
+      address: "Street: 277, I S Sadan X Road, Santoshnagar, Hyderabad, Andhra Pradesh, India",
+      zipcode: '30827',
+      description: 'testing random description',
+    },
+  ];
+  
+  class ViewWarehouse extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+    rows:[{}]
+  };
+  }
+  handleChange = idx => e => {
+    const {name, value} =e.target;
+    const rows = {...this.state.row}
+    rows[idx] ={
+    ...rows[idx],
+    [name]:value
+  };
+  this.setState({
+    rows
+  })
+  }
+  
+  handleEmailChange = idx => e => {
+    const {name, value} =e.target;
+    const rows = {...this.state.row}
+    rows[idx] ={
+    ...rows[idx],
+    email:value
+  };
+  this.setState({
+    rows
+  })
+  }
+  handleAddRow = () => {
+    const item ={
+        email:'',
+        mobile:''
+    }
+  this.setState({
+    rows: [...this.state.rows, item]
+  })
+  }
+  handleRemoveRow = idx =>() =>{
+    const rows = [...this.state.rows];
+    rows.splice(idx,1);
+    this.setState({rows});
+  };
+  render (){
+    const {
+        count = 0,
+        items = [],
+        onDeselectAll,
+        onDeselectOne,
+        onPageChange = () => { },
+        onRowsPerPageChange,
+        onSelectAll,
+        onSelectOne,
+        page = 0,
+        rowsPerPage = 0,
+        selected = [],
+        classes
+      } = this.props;
+      const {rows} =this.state;
+    return (
+      <div style={{minWidth: "100%" }}>
+   <h2>View Warehouse</h2>
+   <Box sx={{  position: 'relative' , overflowX: "auto"}}>    
+        <Scrollbar>
+          <Table sx={{ minWidth: 800,overflowX: "auto" }} columns={columns} dataSource={data}></Table>
+        </Scrollbar>
+      </Box>
+  
+      </div>
+    );
+      }
+  };
+
+ViewWarehouse.propTypes = {
+  customer: PropTypes.object.isRequired
+};
+
+export default ViewWarehouse;
