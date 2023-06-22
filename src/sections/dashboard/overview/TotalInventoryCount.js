cf7819a3db52360193ddb40761621a2df106ba72
Mithesh <mitheshbangera70@gmail.com>
2023-06-22 10:19:17 +0530

    add hyperlink, tooltip info

--- src/sections/dashboard/overview/TotalInventoryCount.js
+++ src/sections/dashboard/overview/TotalInventoryCount.js
@@ -1,13 +1,20 @@
 import PropTypes from 'prop-types';
 import ArrowRightIcon from '@untitled-ui/icons-react/build/esm/ArrowRight';
-import { Box, Button, Card, CardActions, Divider, Stack, SvgIcon, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, Divider, Stack, SvgIcon, Typography, Tooltip } from '@mui/material';
 import ShoppingBag03Icon from 'src/icons/untitled-ui/duocolor/shopping-bag-03';
+import { RouterLink } from 'src/components/router-link';
+import { paths } from 'src/paths';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+
 
 export const TotalInventoryCount = (props) => {
   const { amount } = props;
 
   return (
-    <Card>
+    <Card
+
+    >
+      
       <Stack
         alignItems="center"
         direction={{
@@ -19,6 +26,9 @@ export const TotalInventoryCount = (props) => {
           px: 4,
           py: 3
         }}
+        style={{ textDecoration: 'none' }}
+        component={RouterLink}
+        href={paths.dashboard.inventory.view}
       >
         <div>
         <SvgIcon fontSize="large" color="primary">
@@ -26,19 +36,18 @@ export const TotalInventoryCount = (props) => {
         </SvgIcon>
         </div>
         <Box sx={{ flexGrow: 1 }}>
-          <Typography
-            color="text.secondary"
-            variant="body2"
-          >
-            Total Inventory Count
-          </Typography>
-          <Typography
-            color="text.primary"
-            variant="h4"
-          >
-            {amount}
-          </Typography>
-        </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography color="text.secondary" variant="body2">
+          Total Inventory Count  
+        </Typography>
+        <Tooltip title="This Shows total inventory items. You can view them by clicking this card." arrow>
+          <InfoOutlinedIcon color="primary" fontSize='small' sx={{ml:1}} />
+        </Tooltip>
+      </Box>
+      <Typography color="text.primary" variant="h4">
+        {amount}
+      </Typography>
+    </Box>
       </Stack>
    
     </Card>
--- src/sections/dashboard/overview/TotalWareHouseCount.js
+++ src/sections/dashboard/overview/TotalWareHouseCount.js
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
 import ArrowRightIcon from '@untitled-ui/icons-react/build/esm/ArrowRight';
-import { Box, Button, Card, CardActions, Divider, Stack, SvgIcon, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, Divider, Stack, SvgIcon, Typography , Tooltip} from '@mui/material';
 import ReceiptCheckIcon from 'src/icons/untitled-ui/duocolor/receipt-check';
+import { RouterLink } from 'src/components/router-link';
+import { paths } from 'src/paths';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 export const TotalWareHouseCount = (props) => {
   const { amount } = props;
@@ -19,6 +22,9 @@ export const TotalWareHouseCount = (props) => {
           px: 4,
           py: 3
         }}
+        style={{ textDecoration: 'none' }}
+        component={RouterLink}
+        href={paths.dashboard.invoices.details}
       >
         <div>
         <SvgIcon fontSize="large" color="primary">
@@ -26,19 +32,18 @@ export const TotalWareHouseCount = (props) => {
               </SvgIcon>
         </div>
         <Box sx={{ flexGrow: 1 }}>
-          <Typography
-            color="text.secondary"
-            variant="body2"
-          >
-           Total WareHouse Count
-          </Typography>
-          <Typography
-            color="text.primary"
-            variant="h4"
-          >
-            {amount}
-          </Typography>
-        </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography color="text.secondary" variant="body2">
+          Total Warehouse Count  
+        </Typography>
+        <Tooltip title="This Shows total waehouse items. You can view them by clicking this card." arrow>
+          <InfoOutlinedIcon color="primary" fontSize='small' sx={{ml:1}} />
+        </Tooltip>
+      </Box>
+      <Typography color="text.primary" variant="h4">
+        {amount}
+      </Typography>
+    </Box>
       </Stack>
     
     </Card>
