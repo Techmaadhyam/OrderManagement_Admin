15eebab68a99dc3172db96f43e2f882ca656d2a9
Mithesh <mitheshbangera70@gmail.com>
2023-07-04 21:19:46 +0530

    fix

--- src/sections/dashboard/purchaseorder/edit-purchase-order-form.js
+++ src/sections/dashboard/purchaseorder/edit-purchase-order-form.js
@@ -142,7 +142,7 @@ export const PurchaseOrderEditForm = (props) => {
   //form state handeling
 
   const [type, setType] = useState(state?.type || "");
-  const [quotation, setQuotation] = useState(state?.quotation || "");
+  const [quotation, setQuotation] = useState(state?.quotid || "");
   const [deliveryDate, setDeliveryDate] = useState(
     dayjs(state?.originalDeliveryDate || "")
   );
@@ -748,7 +748,7 @@ export const PurchaseOrderEditForm = (props) => {
           body: JSON.stringify({
             purchaseOrder: {
               id: state?.id,
-              ...(quotation && { quotation: { id: quotation } }),
+              ...(quotation && { quotid: { id: quotation } }),
               ...(tempId && { tempUser: { id: tempId } }),
               ...(userState && { companyuser: { id: userState } }),
               contactPerson: contactName,
